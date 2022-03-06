e67edc5f77f31a380c8a23640477e28502452f4b
Ed Wied <wied.ed@gmail.com>
2022-03-06 13:03:49 -0600

    refactored and added some comments

--- src/app.js
+++ src/app.js
@@ -28,6 +28,9 @@ app.get('/', (req, res) => {
   res.status(200).send("What the Bing?! Noun-importer Service");
 });
 
+//endpoint that sends list of words back to the game
+//if the teammate microservice exists, then it serves words from that service
+//if it doesn't, then it serves up back up list
 app.get('/words', async (req, res) => {
   const doesWordListExist = await doesFileExist();
   listOfWords = [];
@@ -68,12 +71,15 @@ function doesFileExist() {
   });
 }
 
+//returns backup word list that just has the word iphone - this word is not
+//in the teammate service and serves to indicate there's a problem
 function backUpWordList() {
   const wordsToGuess = ["iphone", "iphone", "iphone", "iphone", "iphone"];
   let wordsInNewOrder = _.shuffle(wordsToGuess);
   return wordsInNewOrder;
 }
 
+//gets words list from teammate service
 function getWordList() {
   return new Promise((resolve, reject) => {
     //method to read file adapted from https://nodejs.dev/learn/reading-files-with-nodejs
@@ -88,6 +94,7 @@ function getWordList() {
   });
 }
 
+//converts the file of words to an array - reads file line by line and then returns array 
 function convertListToArray(someList) {
   let returnArray = [];
   //function adapted from https://geshan.com.np/blog/2021/10/nodejs-read-file-line-by-line/
@@ -101,6 +108,7 @@ function convertListToArray(someList) {
   return returnArray;
 }
 
+//gets file system path to word list
 function getPathToWordList() {
   if (RUNNING_LOCAL) {
     return LOCAL_FILE_PATH_TO_WORDLIST;
